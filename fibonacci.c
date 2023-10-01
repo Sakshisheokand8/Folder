d02d7b183463fbfdd50b6e8adf6bd98d17548b87
SannyaJuneja <146001059+SannyaJuneja@users.noreply.github.com>
2023-10-01 16:38:09 +0530

    Wrapping it in a separate function

--- fibonacci.c
+++ fibonacci.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main() {
-    int n = 10, a = 0, b = 1, next;
+void generateFibonacci(int n) {
+    int a = 0, b = 1, next;
     printf("Fibonacci Series: ");
     for (int i = 0; i < n; i++) {
         printf("%d ", a);
@@ -9,5 +9,10 @@ int main() {
         b = next;
     }
     printf("\n");
+}
+
+int main() {
+    int n = 10;
+    generateFibonacci(n);
     return 0;
 }
