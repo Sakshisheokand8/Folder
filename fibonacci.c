c796dad76b0bfc4767106b2b0a6888b87b8ff3b0
Sakshisheokand8 <134862782+Sakshisheokand8@users.noreply.github.com>
2023-09-30 22:57:24 -0400

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-int main {
+int main() {
     int n = 10, a = 0, b = 1, next;
     printf("Fibonacci Series: ");
     for (int i = 0; i < n; i++) {
-        printf("%d ", &a);
+        printf("%d ", a);
         next = a + b;
         a = b;
         b = next;
